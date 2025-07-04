85c33af73dccb698044b2e4d83aeed404d46ffbc
aathifpm <aathifpm123@gmail.com>
2025-07-04 23:51:27 +0530

    Update manage_venues.php

--- admin/manage_venues.php
+++ admin/manage_venues.php
@@ -180,6 +180,7 @@ $venues = fetchVenues($conn);
     <title>Manage Venues - Admin Dashboard</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css">
     <link rel="icon" href="../college_logo.png" type="image/png">
     <style>
@@ -513,7 +514,7 @@ $venues = fetchVenues($conn);
     </div>
 
     <!-- Add Venue Modal -->
-    <div class="modal fade" id="addVenueModal" tabindex="-1" role="dialog" aria-labelledby="addVenueModalLabel" aria-hidden="true">
+    <div class="modal fade" id="addVenueModal" tabindex="-1" role="dialog" aria-labelledby="addVenueModalLabel" aria-hidden="true" style="display: none;">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -641,6 +642,9 @@ $venues = fetchVenues($conn);
 
     <script>
         $(document).ready(function() {
+            // Hide all modals on page load
+            $('.modal').modal('hide');
+            
             $('#venuesTable').DataTable({
                 "order": [[0, "asc"]],
                 "language": {
